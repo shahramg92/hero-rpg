b6120e806d62a3a97c9694a735fc7c9cedd7091b
Shahram Ghassemi <shahramg92@hotmail.com>
2017-09-22 20:45:06 -0500

    new

--- hero_rpg.py
+++ hero_rpg.py
@@ -68,7 +68,7 @@ class Battlemage(Character):
 
 class Nightwitch(Character):
     pass
-    #I want to add something but trying to make my gitb
+    #I want to add something but trying to make my github green daily
 
 
 
